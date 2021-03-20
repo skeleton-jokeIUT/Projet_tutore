e0f9cf14d4dcc4b745a118d410c2bdfa56df7a6c
JOHAN.MAURICE <johan.maurice@etu.univ-lyon1.fr>
2021-03-20 12:29:16 +0100

    Ajout premiere fonction DAO + DTO personne

--- Model/DAO_Personne.php
+++ Model/DAO_Personne.php
@@ -1,1 +1,28 @@
 <?php
+
+class DAOPersonne(){
+
+	private $bdd; 
+
+	public function __construct(){
+		try{
+   			$this->bdd= new PDO(
+   				'mysql:host=localhost;dbname=joueur;charset=utf8',
+   				'Johan',
+   				'1234');
+		}
+
+		catch (Exception $e) {
+     		die('Erreur'.$e->getMessage());
+		}
+	}
+
+	public function getById($i) { #permet de récupérer une ligne d'une personne via l'ID
+ 		$sql = 'SELECT * FROM personne WHERE id_Personne=?';
+		$req = $this->bdd->prepare($sql);
+		$req->execute([$i]);
+		$data = $req->fetch(); 
+		$joueur = new DTOjoueur($i, $data['libelle'], $data['prix']);
+		return $joueur;
+	}
+}
