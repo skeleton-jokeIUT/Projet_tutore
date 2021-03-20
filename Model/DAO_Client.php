f1ddd047f2b2d56460b7a292b91727f732e30f13
JOHAN.MAURICE <johan.maurice@etu.univ-lyon1.fr>
2021-03-20 16:36:02 +0100

    Création DAO et DTO Client

---
+++ Model/DAO_Client.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once('../Model/DTO_Client.php');
+
+class DAOPersonne{
+
+	private $bdd; 
+
+	public function __construct(){
+		try{
+   			$this->bdd= new PDO(
+   				"mysql:host=localhost;dbname=surv'easy;charset=utf8",
+   				'Johan',
+   				'1234');
+		}
+
+		catch (Exception $e) {
+     		die('Erreur'.$e->getMessage());
+		}
+	}
+
+	public function getById($i) { #permet de récupérer une ligne d'une personne via l'ID
+ 		$sql = 'SELECT * FROM client WHERE ID_Client=?';
+		$req = $this->bdd->prepare($sql);
+		$req->execute([$i]);
+		$data = $req->fetch(); 
+		$personne = new DTOPersonne($i, $data['ID_Client'], $data['ID_Personne'], $data['Login'],$data['MDP']);
+		return $personne;
+	}
+}
