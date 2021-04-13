0e908134b6d1bd2be1150ecd38d06d8a60896aa3
johan <johan.maurice@hotmail.fr>
2021-04-13 15:43:41 +0200

    création fonction inscription

--- Controller/index.php
+++ Controller/index.php
@@ -15,38 +15,18 @@ $question = new DAOQuestion();
 $sondage = new DAOSondage();
 $correspondance = new DAOCorrespondance();
 
+$module = 'accueil';
 
-$p1=$personne->getById(1);
-$p2=$personne->getById(2);
+if (isset($_POST['inscription']) $module='inscription';
 
-echo "Utilisateur : ".$p2->__get('id')." <br>nom : ".$p2->__get('nom')."<br>email : ".$p2->__get('mail')."<br>";
+if ($module=='accueil'){
 
-$c1=$client->getById(1);
-$c2=$client->getById(2);
+	include('accueil.html');
+}
 
-echo "<br> Client : <br>ID Client : ".$c1->__get('idClient')." login : ".$c1->__get('login')." mdp : ".$c1->__get('mdp')."<br>";
-echo "ID Client : ".$c2->__get('idClient')."<br>";
-
-$q1=$question->getById(1);
-$q2=$question->getById(2);
-
-echo "<br> Question  : <br>ID question : ".$q1->__get('idQuestion')." Sous-cat :  ".$q1->__get('sousCategorie')." champs : ".$q1->__get('nbChamps')." contenu : ".$q1->__get('contenu')."<br>";
-echo "ID question : ".$q2->__get('idQuestion')." Sous-cat :  ".$q2->__get('sousCategorie')." champs : ".$q2->__get('nbChamps')." contenu : ".$q2->__get('contenu')."<br>";
-
-
-$s1=$sondage->getByNumero(1);
-
-echo "<br> Sondage : <br>ID Client : ".$s1->__get('idClient')." numero Sondage :  ".$s1->__get('numeroSondage')." nombre Question : ".$s1->__get('nombreQuestion')." Date debut ".$s1->__get('dateDebut')."Date Fin : ".$s1->__get('dateFin')."<br>";
-
-
-$cor1=$correspondance->getById(1,1);
-$cor2=$correspondance->getById(1,2);
-
-echo "<br> Correspondance : <br>ID question : ".$cor1->__get('idQuestion')." numero Sondage :  ".$cor1->__get('numeroSondage')."<br>";
-echo "ID question : ".$cor2->__get('idQuestion')." numero SOndage :  ".$cor2->__get('numeroSondage')."<br>";
-
-
-
+if ($module=='inscription'){
+	include('inscription.html');
+}
 
 
 
--- Model/DAO_Client.php
+++ Model/DAO_Client.php
@@ -27,4 +27,24 @@ class DAOClient{
 		$client = new DTOClient($data['ID_Client'], $data['ID_Personne'], $data['Login'], $data['MDP']);
 		return $client;
 	}
+
+	public function inscription($login){
+
+		$sql = 'SELECT Login from client where login=?';
+		$req = $this->bdd->prepare($sql);
+		$req->execute([$login]);
+		$data=$req->fetch;
+		
+		if($data['name']!=$login){
+			echo "inscription ok";
+		}
+		else echo "ce pseudo existe déjà. Merci d'en saisir un autre.";	
+
+	}
+
+	public function connexion(){
+
+
+	}
+
 }
