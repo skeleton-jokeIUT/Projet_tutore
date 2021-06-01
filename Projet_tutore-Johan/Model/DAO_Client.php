da8f0ea0032147133a70f42361100e3a9f10f758
miraclej101 <76498462+miraclej101@users.noreply.github.com>
2021-06-01 19:51:42 +0200

    Add files via upload

---
+++ Projet_tutore-Johan/Model/DAO_Client.php
@@ -0,0 +1,80 @@
+<?php
+
+require_once('../Model/DTO_Client.php');
+
+class DAOClient{
+
+	private $bdd; 
+
+	public function __construct(){
+		try{
+   			$this->bdd= new PDO(
+   				"mysql:host=localhost;dbname=surv'easy;charset=utf8",
+   				'Johan',
+   				'1234');
+		}
+
+		catch (Exception $e) {
+     		die('Erreur'.$e->getMessage());
+		}
+	}
+
+	public function getById($i) { #permet de récupérer une ligne d'une personne via l'ID
+ 		$sql = 'SELECT * FROM client WHERE ID_Client=?';
+		$req = $this->bdd->prepare($sql);
+		$req->execute([$i]);
+		$data = $req->fetch(); 
+		$client = new DTOClient($data['ID_Client'], $data['ID_Personne'], $data['Login'], $data['MDP']);
+		return $client;
+	}
+
+	public function getByLogin($i) { #permet de récupérer une ligne d'une personne via l'ID
+ 		$sql = 'SELECT * FROM client WHERE login=?';
+		$req = $this->bdd->prepare($sql);
+		$req->execute([$i]);
+		$data = $req->fetch(); 
+		$client = new DTOClient($data['ID_Client'], $data['ID_Personne'], $data['login'], $data['MDP']);
+		return $client;
+	}
+
+	public function connexion($login,$mdp){
+
+        $requete ='select * from client where login=?;';
+		$reponse = $this->bdd->prepare($requete);
+		$reponse->execute(array($login));
+		$data = $reponse ->fetch();
+        $reponse->closeCursor();	
+        if($login == $data['login'] && $mdp == $data['MDP'])
+        {
+            return true;
+        }
+        else return false;      
+    }
+
+    public function existClient($email){
+		
+        $requete='select * from client;';
+		$reponse=$this->bdd->query($requete);
+        	while ($data=$reponse ->fetch())
+				{
+					if ($email==$data['Login'])
+					{
+						$reponse->closeCursor();
+                        return true;
+                    }
+				}
+        $reponse->closeCursor();
+		return false;
+	}
+
+		//insert user dans la table Cleint 
+	public function inscription($login,$email,$mdp){
+
+			//une personne pas forcément un client.
+			//$profil=$p_dao->getByEmail($email);	
+			$requete='INSERT into Client (login, MDP, email) VALUES (:t_login, :t_mdp, :t_email);';
+			$req=$this->bdd->prepare($requete);
+			$req->execute(array('t_login'=>$login, 't_mdp'=>$mdp, ':t_email'=>$email));
+		
+	}
+}
---
+++ Projet_tutore-Johan/Vue/accueil.php
@@ -0,0 +1,24 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Accueil Surv'easy</title>
+		<link rel="stylesheet" type="text/css" href="../vue/style.css" />
+	</head>
+	<body>
+
+
+		<form action="index.php" method="post">
+			<legend>Connexion</legend>
+
+			<label>Login :<input type="text" name="login"> </label>
+			<label>mot de passe : <input type="password" name="password"></label>
+			<button name="connexion">Se connecter</button>
+			<button name="inscription">Inscription</button>
+
+		</form>
+
+
+	<a href="index.php?creer_sondage">Créer Sondage</a>	
+
+
+	</body>
