eb88670a4301fae7769f4d696b0594c7211b029e
miraclej101 <76498462+miraclej101@users.noreply.github.com>
2021-06-03 11:32:26 +0200

    Add files via upload

--- Vue/test_reponse.php
+++ Vue/test_reponse.php
@@ -1,9 +1,6 @@
 <?php
 require_once('../Model/DAO_Reponse.php');
-var_dump($_POST['idQuestion']);
-var_dump(($_POST['typeQ']));
-var_dump($idQuestion);
-echo $reponse.PHP_EOL;
+
 ?>
 <div>
      Mercdi de vos reponses.
